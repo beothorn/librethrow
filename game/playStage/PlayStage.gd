808b30ef2499f72e9bac7afc4a1122e21013f456
lucass <lucass@gmail.com>
2020-01-02 22:28:23 +0100

    Click to release ball

--- game/playStage/PlayStage.gd
+++ game/playStage/PlayStage.gd
@@ -1,4 +1,13 @@
 extends Spatial
 
+onready var ball = get_node("/root/GameRoom/Ball")
+
 func _ready():
 	PhysicsServer.area_set_param(get_world().get_space(), PhysicsServer.AREA_PARAM_GRAVITY_VECTOR, Vector3(0, -1, 0))
+
+
+func _on_StaticBody_input_event(camera, event, click_position, click_normal, shape_idx):
+	var mouse_click = event as InputEventMouseButton
+	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed:
+		ball.gravity_scale = 1
+		ball.apply_impulse(Vector3(0,0,0), Vector3(5.5,-5.5,0))
