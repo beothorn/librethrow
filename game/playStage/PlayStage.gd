072ae674b13769552f88f01f18dd88e473fa2956
lucass <lucass@gmail.com>
2020-01-05 19:24:58 +0100

    Restart when all stones are dead

--- game/playStage/PlayStage.gd
+++ game/playStage/PlayStage.gd
@@ -5,8 +5,11 @@ onready var stones = get_node("/root/GameRoom/Stones")
 onready var aim_ball = get_node("/root/GameRoom/Aim_ball")
 onready var ball_gen = preload("res://playStage/ThrownBall.tscn") 
 
-var throw_force = 15
+var throw_force = 8
 var aim_circle_radius = 2.2
+var hidden_z_ccordinate = -1000
+var maximun_aim_angle = -0.26
+var should_restart = false
 
 var throwing = false
 
@@ -15,7 +18,7 @@ func throw_ball(towards_point):
 	root.add_child(ball)
 	ball.connect("collision", self, "_on_ball_hit")
 	ball.translation = aim_ball.translation
-	aim_ball.translation.z = 10
+	aim_ball.translation.z = hidden_z_ccordinate
 	var pos = get_node("Camera").project_ray_origin(towards_point)
 	ball.gravity_scale = 1
 	var throw_direction = Vector3(pos.x, pos.y, 0)
@@ -34,15 +37,25 @@ func _input(event):
 		var pos = get_node("Camera").project_ray_origin(event.position)
 		var pos2d = Vector3(pos.x, pos.y, 0)
 		var pos_cursor = pos2d.normalized() * aim_circle_radius
-		aim_ball.translation.x = pos_cursor.x
-		aim_ball.translation.y = pos_cursor.y
+		if pos_cursor.y < maximun_aim_angle:
+			aim_ball.translation.x = pos_cursor.x
+			aim_ball.translation.y = pos_cursor.y
 			
 func _on_ball_hit(obj, ball):
 	if obj.is_in_group("Stone"):
 		#removing child is slow so we just hide it
-		obj.translation.z=-1000
+		obj.translation.z = hidden_z_ccordinate
+		should_restart = true
+		for stone in stones.get_children():
+			if stone.translation.z != hidden_z_ccordinate:
+				should_restart = false
+		
 	if obj.is_in_group("BallKiller"):
 		root.remove_child(ball)
 		throwing = false
 		aim_ball.translation.z = 0
+		if should_restart:
+			for stone in stones.get_children():
+				stone.translation.z = 0
+			should_restart = false
 	
